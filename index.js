30cf310faf7f0f036c3cf56a6eef22904d28eec2
missmelisande <136761195+missmelisande@users.noreply.github.com>
2023-07-13 09:37:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.get('/gpt/:text', async (req, res) => {
       console.log("User Input: " + text)
 
       const response = await openai.createChatCompletion({
-        model: "gpt-4",
+        model: "gpt-3.5-turbo-16k",
         messages: messages,
         temperature: 0.5,
         max_tokens: 256,
